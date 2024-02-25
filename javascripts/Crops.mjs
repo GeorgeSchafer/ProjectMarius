3f942d9985cee10658111a3640dd82ef4e98e1f3
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-02-24 19:01:17 -0700

    reorganized assets

--- javascripts/Crops.mjs
+++ javascripts/Crops.mjs
@@ -1,4 +1,3 @@
-
 class Crop {
     constructor(name){
         this.timeStamp = Crop.stampTime()
@@ -10,35 +9,6 @@ class Crop {
 
     static stampTime(){
         return new Date()
-    }
-
-    static incrementStage(crops){    
-        const now = Crop.stampTime()
-
-        Object.values(crops).forEach(value => {
-            value.forEach(crop => {
-                if(!crop.getMature() && this.timeStamp - now > crop.time[crop.stage]){
-                    crop.timeStamp = now
-                    crop.stage++
-                    crop.stage === crop.time.length
-                        ? crop.mature = true 
-                        : crop.mature = false
-                    console.log(`The ${crop.name} is now at stage ${crop.stage} of ${crop.time.length}`)
-                }
-            })
-        })
-    }
-
-    static allMature(crops){
-        Object.values(crops).forEach(value => {
-            value.forEach(crop => {
-                if(!crop.getMature()){
-                    return false
-                }
-            })
-        })
-        
-        return true
     }
 
     getMature(){
@@ -54,11 +24,26 @@ class Cabbage extends Crop {
     constructor(name='cabbage'){
         super(name)
         this.time = [
-            120000, 
             60000,
             30000,
-            15000
+            15000,
+            10000
         ]
+    }
+}
+
+class Cauliflower extends Cabbage {
+    constructor(name='cauliflower'){
+        super(name)
+        animation_tile_ids = [1279]
+    }
+
+}
+
+class Lettuce extends Cabbage {
+    constructor(name='lettuce'){
+        super(name)
+        animation_tile_ids = [1644]
     }
 }
 
@@ -66,13 +51,29 @@ class Grain extends Crop {
     constructor(name='grain'){
         super(name)
         this.time = [
-            60000,
-            40000,
             30000,
             15000,
-            10000
+            10000,
+            7500,
+            3750
+
         ]
     }
+}
+
+class Maize extends Grain {
+    constructor(name='maize'){
+        super(name)
+        animation_tile_ids = [1524,1483]
+    }
+}
+
+class Wheat extends Grain {
+    constructor(name='wheat'){
+        super(name)
+        animation_tile_ids = [1442,1401]
+    }
+
 }
 
 class Grape extends Crop {
@@ -89,6 +90,20 @@ class Grape extends Crop {
     }
 }
 
+class RedGrape extends Grape {
+    constructor(name='red grape'){
+        super(name)
+        animation_tile_ids = [1606,1565,1361]
+    }
+}
+
+class WhiteGrape extends Grape {
+    constructor(name='white grape'){
+        super(name)
+        animation_tile_ids = [1606,1565,1357]
+    }
+}
+
 class Melon extends Crop {
     constructor(name='melon'){
         super(name)
@@ -100,13 +115,146 @@ class Melon extends Crop {
             15000,
             7500
         ]
+        animation_tile_ids = [1320]
+    }
+}
+
+class Potato extends Crop {
+    constructor(name='potato'){
+        super(name)
+        this.time = [
+            60000,
+            40000,
+            30000,
+            15000,
+            7500
+        ]
+    }
+
+}
+
+class Root extends Crop {
+    constructor(name='root'){
+        super(name)
+        this.time = [
+            30000,
+            15000,
+            7500
+        ]
+    }
+}
+
+class Carrot extends Root {
+    constructor(name='carrot'){
+        super(name)
+        animation_tile_ids = [1237]
+    }
+}
+
+class Turnip extends Root {
+    constructor(name='turnip'){
+        super(name)
+        animation_tile_ids = [1155]
+    }
+}
+
+class Farm {
+    constructor(){
+        this.crops = {
+            cabbages: [],
+            grains: [],
+            grapes: [],
+            melons: [],
+            roots: [],
+            potatoes: [],
+            beans: [],
+            trees: []
+        }
+        this.fields = []
+    }
+
+    maturityCheck(){
+        /**
+         * @param {Object} this.crops - An object containing arrays of Crop objects
+         *      crops = {
+         *         cabbages: [cabbage1, cabbage2, ...],
+         *         ...
+         *      }
+         */
+        let result = true
+        Object.values(this.crops).forEach(value => {
+            value.forEach(crop => {
+                if(!crop.getMature()){
+                    result = false
+                }
+            })
+        })
+        
+        return result
+    }
+
+    incrementStage(){
+        const now = Crop.stampTime()
+
+        Object.values(this.crops).forEach(value => {
+            value.forEach(crop => {
+                if(!crop.getMature() && this.timeStamp - now > crop.time[crop.stage]){
+                    crop.timeStamp = now
+                    crop.stage++
+                    crop.stage === crop.time.length
+                        ? crop.mature = true 
+                        : crop.mature = false
+                    console.log(`The ${crop.name} is now at stage ${crop.stage} of ${crop.time.length}`)
+                }
+            })
+        })
+    }
+}
+
+class Field {
+    constructor(){
+        /**
+         * @param {Array} this.plots - is an array of arrays that represent the field
+         *     by their index. Each plot is a 1x1 square of land.
+         */
+        this.plot = [
+            []
+        ]
+        this.health = 4
+    }
+
+    getPlot(x, y=0){
+        return this.plot[x][y]
+    }
+
+    setPlot(crop, x, y=0){
+        this.plot[x][y] = crop
     }
 }
 
 export {
     Crop,
+
     Cabbage,
+    Cauliflower,
+    Lettuce,
+
     Grain,
+    Maize,
+    Wheat,
+    
     Grape,
-    Melon
+    RedGrape,
+    WhiteGrape,
+    
+    Melon,
+
+    Potato,
+    
+    Root,
+    Carrot,
+    Turnip,
+    
+    Farm,
+    Field
 }
--- test/Marius.spec.mjs
+++ test/Marius.spec.mjs
@@ -22,13 +22,9 @@ import { expect } from 'chai'
 
 
 describe('Crops', () => {
-    describe('Constructor', () => {
+    describe('Crop Constructor', () => {
         const start = new Date()
         const crop = new Crop('rice')
-        const cabbage = new Cabbage()
-        const grain = new Grain()
-        const grape = new Grape()
-        const melon = new Melon()
         const finish = new Date()
 
         valueMatch(crop.name, 'rice')
@@ -36,58 +32,9 @@ describe('Crops', () => {
         valueMatch(crop.time, [])
         valueMatch(crop.getMature(), false)
         testStamp(start, finish, crop)
-
-        valueMatch(cabbage.name, 'cabbage')
-        valueMatch(cabbage.stage, 0)
-        valueMatch(cabbage.time, [
-            120000, 
-            60000,
-            30000,
-            15000
-        ])
-        valueMatch(cabbage.getMature(), false)
-        testStamp(start, finish, cabbage)
-
-        valueMatch(grain.name, 'grain')
-        valueMatch(grain.stage, 0)
-        valueMatch(grain.time, [
-            60000,
-            40000,
-            30000,
-            15000,
-            10000
-        ])
-        valueMatch(grain.getMature(), false)
-        testStamp(start, finish, grain)
-
-        valueMatch(grape.name, 'grape')
-        valueMatch(grape.stage, 0)
-        valueMatch(grape.time, [
-            120000,
-            90000,
-            60000,
-            30000,
-            15000,
-            7500
-        ])
-        valueMatch(grape.getMature(), false)
-        testStamp(start, finish, grape)
-
-        valueMatch(melon.name, 'melon')
-        valueMatch(melon.stage, 0)
-        valueMatch(melon.time, [
-            120000,
-            90000,
-            60000,
-            30000,
-            15000,
-            7500
-        ])
-        valueMatch(melon.getMature(), false)
-        testStamp(start, finish, melon)
     })
 
-    describe('Crop subclasses', () => {
+    describe('Crop subclass Constructors', () => {
         const start = new Date()
         const crops = {
             melons: [
